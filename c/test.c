29978454599407b3382533bd7e8523948337a364
x9t <imnxtgxxd@gmail.com>
2025-08-15 00:04:24 +0100

    s

--- c/test.c
+++ c/test.c
@@ -1,58 +1,30 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
-char* crypter(char* input){
+int bs(int tab[100],int target){
+    int d = 0;
+    int f = 16;
 
-  char* key = "keyhash";
-  int passL = strlen(input);
-  int keyL = strlen(key);
-  char* result = (char*)malloc(passL + 1);
-  char* origin = (char*)malloc(passL + 1);
- 
+    while (d <= f){
+        int mid = (d+f)/2;
 
-  for(int i = 0 ; i < passL ; i++){
-  
-    result[i] = input[i] ^ key[i % keyL];  
-
-  }
-  result[passL] = '\0';
-
-  return result;
-
-}
-
-char * decrypter(char* hashed){
-
-  char* key = "keyhash";
-  int passL = strlen(hashed);
-  int keyL = strlen(key);
-  char* origin = (char*)malloc(passL + 1);
-  for(int i = 0 ; i < passL ; i++){
-    origin[i] = hashed[i] ^ key[i % keyL];  
-  }
-
-  origin[passL] = '\0';
-  return origin;
-
-
-}
-int main(){
-    char inputt[100];
-
-    printf("Enter input : \n");
-    scanf("%s",inputt);
+        if (tab[mid] == target){
+            return mid;
+        }
+        if (tab[mid] > target){
+            f = mid - 1;
+            
+        }else{
+            d = mid+1;
+        }
+        
+    }
+  return -1;
     
-    char* test = crypter(inputt);
-
-    printf("%s \n",test);
-
-    char* fixed = decrypter(test);
-
-    printf("%s",fixed);
-
-    free(test);
-    free(fixed);
-
-    return 0 ;
+}
+int main() {
+    
+int table[17] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    
+    printf("%d",bs(table,90));
+    return 0;
 }
